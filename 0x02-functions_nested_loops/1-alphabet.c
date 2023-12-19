6dff9391b3bf92c32b8a543d79f8b35b2376f824
ogadarita <rita.writescontent@gmail.com>
2023-12-19 02:44:08 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet - Function that prints alphabets in lowecase
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
