8fdf7d75355b374ca9c1c8fdd46102779b836ecf
ogadarita <rita.writescontent@gmail.com>
2023-12-22 23:19:47 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,14 +12,13 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	int leapYear = (year % 4 && (year % 100 != 0 || year % 400 == 0));
-
-	if (leapYear)
+	if ((year % 4 == 0 || year % 400 == 0))
 	{
-		if (month > 2)
+		if (month >= 2 && day >= 60)
 		{
 			day++;
 		}
+        	
         	printf("Day of the year: %d\n", day);
         	printf("Remaining days: %d\n", 366 - day);
         }
