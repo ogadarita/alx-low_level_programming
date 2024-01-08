23c9271c5ccdbb0b1162a3bb0e920c94b6321d7a
ogadarita <rita.writescontent@gmail.com>
2024-01-08 03:00:51 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stddef.h>
+
+#include <stddef.h>
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n > 0)
+	{
+		*s = b;
+		s++;
+		n--;
+	}
+	return (ptr);
+}
