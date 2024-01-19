a1c4a5edc5af011a79aa84c9c249a85c456a814f
ogadarita <rita.writescontent@gmail.com>
2024-01-18 18:24:12 -0800

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,36 +16,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *result;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	while (s2[len2] != '\0')
-	{
+	for (i = 0; s1[i] != '\0'; i++)
+		len1++;
+	for (i = 0; s2[i] != '\0'; i++)
 		len2++;
-	}
+	result = malloc(sizeof(char) * (len1 + n) + 1);
+	if (result == NULL)
+		return (NULL);
 	if (n >= len2)
 	{
-		n = len2;
+		for (i = 0; s1[i] != '\0'; i++)
+			result[i] = s1[i];
+		for (i = 0; s2[i] != '\0'; i++)
+			result[len1 + i] = s2[i];
+		result[len1 + i] = '\0';
 	}
-	result = malloc((len1 + n + 1) * sizeof(char));
-
-	if (result == NULL)
+	else
 	{
-		return (NULL);
+		for (i = 0; s1[i] != '\0'; i++)
+			result[i] = s1[i];
+		for (i = 0; i < n; i++)
+			result[len1 + i] = s2[i];
+		result[len1 + i] = '\0';
 	}
-	for (i = 0; i < len1; i++)
-	{
-		result[i] = s1[i];
-	}
-	for (i = 0; i < n; i++)
-	{
-		result[len1 + i] = s2[i];
-	}
-	result[len1 + n] = '\0';
-
 	return (result);
 }
