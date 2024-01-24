800f02b6f695edb57ddd1d3cca77b7fa95fc7d7a
ogadarita <rita.writescontent@gmail.com>
2024-01-24 01:27:20 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - a structure defining dog's features
+* @name: pointer to name of dog
+* @age: age of dog
+* @owner: pointer to owner of dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
