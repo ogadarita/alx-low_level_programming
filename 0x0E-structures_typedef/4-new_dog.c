4420b01487642c7e8dbb5ef5ad52ba16534fd03d
ogadarita <rita.writescontent@gmail.com>
2024-01-25 05:36:06 -0800

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* new_dog - function that returns a pointer to a type dog_t
+* @name: pointer to name of dog
+* @age: age of dog
+* @owner: pointer to owner of the dog
+* Return: new_dog
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (new_dog != NULL)
+	{
+		new_dog->name = (char *)malloc(strlen(name) + 1);
+		new_dog->owner = (char *)malloc(strlen(owner) + 1);
+	}
+	if (new_dog->name != NULL && new_dog->owner != NULL)
+	{
+		strcpy(new_dog->name, name);
+		strcpy(new_dog->owner, owner);
+		new_dog->age = age;
+	}
+	else
+	{
+		printf("Error: Cannot allocate memory for new_dog name or owner\n");
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+
+		return (NULL);
+	}
+	printf("Error: Cannot allocate memory for new_dog\n");
+
+	return (new_dog);
+}
