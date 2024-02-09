8b01a94fdfac2e0701037ca1e22ead7e6e055724
ogadarita <rita.writescontent@gmail.com>
2024-02-09 02:08:51 -0800

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,8 +2,8 @@
 
 /**
 * set_bit - sets the value of a bit to 1 at a given index
-* n: pointer to unsigned long int to modify
-* index: index of the bit to set, starting from 0
+* @n: pointer to unsigned long int to modify
+* @index: index of the bit to set, starting from 0
 * Return: 1 (Success), or -1 (Error)
 */
 
