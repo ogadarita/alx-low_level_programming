0da5316a90dd6948265e96229b364e88967857d9
ogadarita <rita.writescontent@gmail.com>
2024-02-10 02:04:37 -0800

    simple operations using function pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -47,7 +47,16 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-	return (a / b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	else
+	{
+		return (a / b);
+	}
+
 }
 
 /**
@@ -59,6 +68,13 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	return (a % b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	else
+	{
+		return (a % b);
+	}
 }
-
